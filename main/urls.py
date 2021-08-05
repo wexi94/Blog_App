665f44e3dbecaf79e49cb710d7b76aff7135e85b
wexi94 <muhammadawais2512@gmail.com>
2021-08-05 20:10:48 +0500

    Second Commit

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import render_pdf_view,UserListView,user_render_pdf_view
 urlpatterns=[
     path('',views.home,name='home'),
     path('accounts/register/',views.register,name='register'),
     path('reset_password/',auth_views.PasswordResetView.as_view(), name='reset_password'),
     path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('pdf_test/', render_pdf_view, name='test-view'),
+    path('users_test/', UserListView.as_view() , name='users_list'),
+    path('users/<pk>/', user_render_pdf_view, name='users_pdf_view'),  
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.contrib import messages
 #from django.contrib.auth.forms import UserCreationForm
 from .forms import SignupForm
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.views.generic import ListView
+from django.contrib.auth.models import User
+
+class UserListView(ListView):
+    model = User
+    template_name = 'main/list.html'
+
+def user_render_pdf_view(request, *args, **kwargs):
+    pk = kwargs.get('pk')
+    user = get_object_or_404(User, pk=pk)
+
+    template_path = 'main/pdf2.html'
+    context = {'user': user}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    # if we want to download the Report Automatically
+    #response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    #if you want to view the report in the browser
+    response['Content-Disposition'] = ' filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response,)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+def render_pdf_view(request):
+    template_path = 'main/pdf1.html'
+    context = {'myvar': 'this is your template context'}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    # if we want to download the Report Automatically
+    #response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    #if you want to view the report in the browser
+    response['Content-Disposition'] = ' filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response,)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def home(request):
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
